5a984116f0b6f5e0743209749a0bc9de3371003f
gekas920 <valeryanka77777@gmail.com>
2019-12-11 19:05:19 +0300

    Navbar

--- src/NavigationBar/NavigationBar.js
+++ src/NavigationBar/NavigationBar.js
@@ -4,15 +4,25 @@ import './NavigationBar.css'
 
 class NavBar extends React.Component
 {
+
+    handleClick = event=>{
+      let arr = document.querySelectorAll('.navbar--elem');
+      let elem = event.target;
+      arr.forEach(elem=>{
+         elem.style.backgroundColor = '';
+      });
+      elem.style.backgroundColor = 'whitesmoke';
+    };
+
     render() {
         return(
             <div>
                 <header>
                     <div className='navbar'>
                         <ul>
-                            <li><Link to='/tasks' className='navbar--elem'>Tasks</Link></li>
-                            <li><Link to='/my-tasks' className='navbar--elem'>My Tasks</Link></li>
-                            <li><Link to='/profile' className='navbar--elem'>My Profile</Link></li>
+                            <li><Link to='/tasks' className='navbar--elem' onClick={this.handleClick}>Tasks</Link></li>
+                            <li><Link to='/my-tasks' className='navbar--elem' onClick={this.handleClick}>My Tasks</Link></li>
+                            <li><Link to='/profile' className='navbar--elem' onClick={this.handleClick}>My Profile</Link></li>
                         </ul>
                     </div>
                 </header>
