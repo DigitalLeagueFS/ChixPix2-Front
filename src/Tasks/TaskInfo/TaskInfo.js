1066f8ea095cb4a493558e800146f3d97193cb34
gekas920 <valeryanka77777@gmail.com>
2020-01-30 14:35:11 +0300

    userTasks

--- src/Tasks/TaskInfo/TaskInfo.js
+++ src/Tasks/TaskInfo/TaskInfo.js
@@ -61,31 +61,33 @@ class TaskInfo extends React.Component{
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(this.props !== prevProps){
-            Request.get(`/tasks/${this.props.id}`)
-                .then(response=>{
-                    if(response){
-                        this.setState({
-                            id:this.props.id,
-                            name:response.data.name,
-                            description:response.data.description,
-                            beginning:response.data.beginning,
-                            deadline:response.data.deadline,
-                            status:response.data.result_description,
-                            executor:{
-                                name:response.data.user_firstname,
-                                surname:response.data.user_secondname,
-                                thirdName:response.data.user_thirdname
-                            },
-                            clientInfo:{
-                                name:response.data.client_name,
-                                surname:response.data.client_surname,
-                                thirdName:response.data.client_thirdname
-                            },
-                            disabled:false,
-                            disabledTask:false
-                        })
-                    }
-                });
+            if(this.props.show){
+                Request.get(`/tasks/${this.props.id}`)
+                    .then(response=>{
+                        if(response){
+                            this.setState({
+                                id:this.props.id,
+                                name:response.data.name,
+                                description:response.data.description,
+                                beginning:response.data.beginning,
+                                deadline:response.data.deadline,
+                                status:response.data.result_description,
+                                executor:{
+                                    name:response.data.user_firstname,
+                                    surname:response.data.user_secondname,
+                                    thirdName:response.data.user_thirdname
+                                },
+                                clientInfo:{
+                                    name:response.data.client_name,
+                                    surname:response.data.client_surname,
+                                    thirdName:response.data.client_thirdname
+                                },
+                                disabled:!!response.data.user_firstname,
+                                disabledTask:!!response.data.user_firstname
+                            })
+                        }
+                    });
+            }
         }
     }
 
@@ -137,50 +139,50 @@ class TaskInfo extends React.Component{
     render() {
         return(
             <div className={this.props.show ? 'tasks-info-box tasks-info-box-show' : 'tasks-info-box'}>
-                <div>
-                    <button className='companies-info-box-btn'
-                            onClick={this.deleteTask}
-                            disabled={this.state.disabled}>
-                        <DeleteForeverIcon/>
-                    </button>
-                </div>
-                <div className='tasks-info-box-content'>
-                    <h1 style={{letterSpacing: '3px',textTransform:'uppercase'}}>{this.state.name}</h1>
-                    {this.contentInfo('exe')}
-                    {this.contentInfo('client')}
-                    {this.contentInfo('beginning')}
-                    {this.contentInfo('deadline')}
-                    {this.contentInfo('status','orange')}
-                    <div className='box-companies-info-desc' style={{width:'70%'}}>
-                        <ExpansionPanel style={{minWidth:'70%',margin: '0 auto'}}>
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>Description</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Typography style={{overflow:'scroll',maxHeight:'300px'}}>
-                                    {this.state.description}
-                                </Typography>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
+                    <div>
+                        <button className='companies-info-box-btn'
+                                style={this.state.disabledTask || this.state.executor.name ? {backgroundColor:'grey'}:{}}
+                                onClick={this.deleteTask}
+                                disabled={this.state.disabled}>
+                            <DeleteForeverIcon/>
+                        </button>
                     </div>
-                    <button className='client-btn'
-                            style={Object.assign({},
-                                {marginTop:'10%'},this.state.disabledTask || this.state.executor.name ? {backgroundColor:'grey'}:{}
+                    <div className='tasks-info-box-content'>
+                        <h1 style={{letterSpacing: '3px',textTransform:'uppercase'}}>{this.state.name}</h1>
+                        {this.contentInfo('exe')}
+                        {this.contentInfo('client')}
+                        {this.contentInfo('beginning')}
+                        {this.contentInfo('deadline')}
+                        {this.contentInfo('status','orange')}
+                        <div className='box-companies-info-desc' style={{width:'70%'}}>
+                            <ExpansionPanel style={{minWidth:'70%',margin: '0 auto'}}>
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography>Description</Typography>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <Typography style={{overflow:'scroll',maxHeight:'300px'}}>
+                                        {this.state.description}
+                                    </Typography>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        </div>
+                        <button className='client-btn'
+                                style={Object.assign({},
+                                    {marginTop:'10%'},this.state.disabledTask || this.state.executor.name ? {backgroundColor:'grey'}:{}
                                 )}
-                            onClick={this.takeTask}
-                            disabled={this.state.disabledTask}>
-                        Take task
+                                onClick={this.takeTask}
+                                disabled={this.state.disabledTask}>
+                            Take task
+                        </button>
+                    </div>
+                    <button className={'back-btn'} onClick={() => {this.props.updateShowInfo(!this.props.show);this.clearState()}}>
+                        <ArrowIcon/>
                     </button>
                 </div>
-                <button className={'back-btn'} onClick={() => {this.props.updateShowInfo(!this.props.show);this.clearState()}}>
-                    <ArrowIcon/>
-                </button>
-
-            </div>
         )
     }
 }
