5e1c9cd3a80baf2b85e46b18f48ee1aa010f28d1
Tsukumogamiru <Mim_gogo@mail.ru>
2020-02-12 19:57:09 +0300

    Поправил добавление клиента

--- src/Clients/AddClient/ClientDialog/ClientDialog.js
+++ src/Clients/AddClient/ClientDialog/ClientDialog.js
@@ -142,58 +142,35 @@ class ClientDialog extends React.Component{
         return <option value = {name} key={name}>{name}</option>
     };
 
-    mapOption(){
-        return this.state.company.map(value => {
-            return this.getOption(value.company_name)
-        })
-    };
+    inputField(stateField,userField){
+        return(
+            <p>
+                <label>
+                    {userField}
+                    <input className='clients-input' type={'text'} name={stateField} placeholder={`Enter ${userField}`}
+                           onChange={this.handleChange(stateField)}/>
+                </label>
+            </p>
+        )
+    }
 
     render() {
         return(
-            <div className='container-dialog'>
+            <div className='container-dialog'  style={{height:'900px'}}>
                 <div className='form-dialog'>
+
+                    {this.inputField('name','Name')}
+                    {this.inputField('surname','SurName')}
+                    {this.inputField('thirdName','Third Name')}
+                    {this.inputField('company','Company')}
+                    {this.inputField('phone','Phone')}
+                    {this.inputField('date','Date')}
+                    {this.inputField('link','Link to Photo')}
                     {this.state.show && <Chip label={this.state.content} color="secondary" style={{width:'100%'}}/>}
-                   <div>
-                                Name:
-                                <input className='clients-input' type={'text'} name={'name'} placeholder={'Enter name'}
-                                       value={this.state.name} onChange={this.handleChange('name')}/>
-                    </div>
                     <div>
-                                Surname:
-                                <input className='clients-input' type={'text'} name={'surname'} placeholder={'Enter Surname'}
-                                       value={this.state.surname} onChange={this.handleChange('surname')}/>
-                    </div>
-                    <div>
-                                Third Name:
-                                <input className='clients-input' type={'text'} name={'thirdName'} placeholder={'Enter Third Name'}
-                                       value={this.state.thirdName} onChange={this.handleChange('thirdName')}/>
-                    </div>
-                    <div>
-                                Company:
-                                <select className='clients-input' name={'company'} placeholder={'Enter Name of Company'}
-                                         defaultValue='DigitalLeague' onChange={this.handleChange('clickedCompany')}>
-                                    {this.mapOption()}
-                                </select>
-                    </div>
-                    <div>
-                                Phone:
-                                <input className='clients-input' type={'phone'} name={'phone'} placeholder={'Enter Phone'}
-                                       value={this.state.phone} onChange={this.handleChange('phone')}/>
-                    </div>
-                    <div>
-                                Date:
-                                <input className='clients-input' type={'date'} name={'date'} placeholder={'Enter Date'}
-                                       value={this.state.date} onChange={this.handleChange('date')}/>
-                    </div>
-                    <div>
-                                Link to Photo:
-                                <input className='clients-input' type={'text'} name={'link'} placeholder={'Enter Link'}
-                                       value={this.state.link} onChange={this.handleChange('link')}/>
-                    </div>
-                    <div>
-                            <button className={'clients-btn'} onClick={this.sendData} disabled={this.state.disabled}>
-                                Create client
-                            </button>
+                        <button className={'clients-btn'} onClick={this.sendData} disabled={this.state.disabled}>
+                            Create client
+                        </button>
                     </div>
                 </div>
             </div>
